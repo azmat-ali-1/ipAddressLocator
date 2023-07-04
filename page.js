4f47a076d21036279fcebffb531426716eea27fa
AZMAT ALI <222azmat@gmail.com>
2023-07-04 23:06:59 +0530

    s

--- page.js
+++ page.js
@@ -5,7 +5,7 @@ function clearDetail(){
 }
 function addDetails(data){
     let postOffice = data;
-    console.log(data);
+  
     let container = document.getElementById("details");
     for(let i=0;i<postOffice.length;i++){
         let div = document.createElement("div");
@@ -48,9 +48,9 @@ function SerachDetail(){
  
 function addMap(data){
     let div  = document.getElementById("map");
-    console.log(data);
+
     div.innerHTML=
-    `<iframe src="https://maps.google.com/maps?q=${data.lat},${data.lon}&z=15&output=embed" width="100%" height="400" frameborder="0" style="border:0"></iframe>`;
+    `<iframe src="https://maps.google.com/maps?q=${data.loc}&z=15&output=embed" width="100%" height="400" frameborder="0" style="border:0"></iframe>`;
 
 }
 function addMessage(data){
@@ -63,7 +63,6 @@ function addMessage(data){
 }
 async function numberOfPincode(zip){
 try {
-    console.log(zip)
     // let response = await fetch(`https://ipinfo.io/${zip}/geo`);
     let response = await fetch(`https://api.postalpincode.in/pincode/${zip}`);
     let data = await response.json();
@@ -83,36 +82,37 @@ zone=  date.toLocaleString('en-US', {
     });
     
    
-    let msg = numberOfPincode(jsonData.zip);
+    let msg = numberOfPincode(jsonData.postal);
     let list = document.getElementById("list");
     list.innerHTML=` <li>Time Zone : <span>${jsonData.timezone}</span></li>
     <li>Date And Time : <span>${zone}</span></li>
-    <li>Pincode : <span>${jsonData.zip}</span></li>`;
+    <li>Pincode : <span>${jsonData.postal}</span></li>`;
 }
  
 function loadData(jsonData){
     let table = document.getElementById("table");
-    console.log(jsonData)
+    let str = jsonData.loc;
+    str = str.split(",");
     table.innerHTML=` <tr>
     <td>
-        Lat : <span>${jsonData.lat}</span>
+        Lat : <span>${str[0]}</span>
     </td>
     <td>
         City : <span>${jsonData.city}</span>
     </td>
     <td>
-        Organisation : <span>${jsonData.org}</span>
+        Organisation : <span>${jsonData.asn.name}</span>
     </td>
 </tr>
 <tr>
     <td>
-    Lag : <span>${jsonData.lon}</span>
+    Lag : <span>${str[1]}</span>
     </td>
     <td>
         Region : <span>${jsonData.region}</span>
     </td>
     <td>
-        Hostname : <span>${jsonData.as}</span>
+        Hostname : <span>${jsonData.hostname}</span>
     </td>
 </tr>`;
 addMap(jsonData);
@@ -123,10 +123,12 @@ const accessdata = async ()=>{
     let ip = document.cookie;
     let az = document.getElementById("ip");
     az.innerText=ip;
-    console.log(ip);
     try {
-    let response = await fetch(`https://ip-api.com/json/${ip}`);
+  
+    let response = await fetch(`https://ipinfo.io/${ip}/?token=06a5f04c1f0c0e`);
+   
     let jsonData = await response.json();
+   
     loadData(jsonData);
     } catch (error) {
         console.log(error,"Access the data");
